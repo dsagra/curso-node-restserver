3abcbde2f16c0747dca8295dd51e3ba797c91465
Damian Sagranichne <dsagra@gmail.com>
2022-10-11 14:34:35 -0300

    Google Sign in

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
@@ -11,6 +11,11 @@ router.post('/login', [
     check('password', 'Password is required').not().isEmpty(),
     validarCampos,
 ], login);
+
+router.post('/google', [
+    check('id_token', 'Id_token is required').not().isEmpty(),
+    validarCampos,
+], googleSignIn);
 
 
 module.exports = router;
